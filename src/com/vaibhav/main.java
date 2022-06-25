90c148ba6bbe7d3e4dc94d4fcadc0b56d2864af8
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-06-25 14:25:59 +0530

    new java file created

---
+++ src/com/vaibhav/main.java
@@ -0,0 +1,11 @@
+package com.vaibhav;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.print("Please enter your roll number: ");
+        Scanner input=new Scanner(System.in);
+        System.out.println("Your roll number is"+" "+input.nextInt());
+    }
+}
