e495f5330dbe58e54ad06de33a2bd9019fb32d34
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-06-29 13:07:32 +0530

    new java file created

---
+++ src/com/vaibhav/typecasting.java
@@ -0,0 +1,14 @@
+package com.vaibhav;
+
+import java.util.Scanner;
+
+public class typecasting {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        //typecasting
+        int num;
+        num = (int) input.nextFloat();
+        System.out.println(num);
+        //automatic type promotion is expressions
+    }
+}
