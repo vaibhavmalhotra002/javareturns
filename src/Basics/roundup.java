ab524b5811de5e708ae68345b67cf54dde0f184b
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-07-06 23:42:40 +0530

    roundoff

---
+++ src/Basics/roundup.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class roundup {
+    public class Decimal {
+
+        public static void main(String[] args) {
+            double num = 1.34567;
+
+            System.out.format("%.2f", num);
+        }
+    }
+}
