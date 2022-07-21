4769cd8f30c956e1728fc854dec79c7430b9ef96
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-07-21 20:51:01 +0530

    sum

--- src/functions/Main.java
+++ src/functions/Main.java
@@ -6,9 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         //Q: Take input of two numbers and print the sum
-        sum();
+        int ans =sum2();
+        System.out.println(ans);
     }
-
+static int sum2(){
+    Scanner in=new Scanner(System.in);
+    System.out.print("Enter number 1:");
+    int num1= in.nextInt();
+    System.out.print("Enter number 2:");
+    int num2= in.nextInt();
+    int sum=num1 +num2;
+    return sum;
+}
 
     static void sum(){
         Scanner in=new Scanner(System.in);
